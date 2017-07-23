f8bb97c11bfe1c5b5369c8fbea8c47eb8727758b
Aditya Narayan Shahi <adityan.shahi2014@gmail.com>
2017-07-23 16:54:47 +0530

    Updated aboutme

--- app/src/main/java/com/example/lenovo/scarnersdice/aboutme.java
+++ app/src/main/java/com/example/lenovo/scarnersdice/aboutme.java
@@ -4,16 +4,40 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class aboutme extends AppCompatActivity {
     Button fb;
+    TextView email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutme);
         fb= (Button)findViewById(R.id.button11);
+        email = (TextView)findViewById(R.id.textView18);
+
+        SpannableString content = new SpannableString("adityan.shahi2014@gmail.com");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+
+
+        email.setText(content);
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailintent = new Intent(android.content.Intent.ACTION_SEND);
+                emailintent.setType("plain/text");
+                emailintent.putExtra(android.content.Intent.EXTRA_EMAIL,new String[] {"adityan.shahi2014@gmail.com" });
+                emailintent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+                emailintent.putExtra(android.content.Intent.EXTRA_TEXT,"");
+                startActivity(Intent.createChooser(emailintent, "Send mail..."));
+            }
+        });
+
         fb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,5 +47,13 @@ public class aboutme extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent j = new Intent(aboutme.this,mpage.class);
+        startActivity(j);
+        finish();
     }
 }
